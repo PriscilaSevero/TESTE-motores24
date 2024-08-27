d4463311c9d3df9ab76063282bdb1eafd4be3fb7
PriscilaSevero <priscila.severo@escolar.ifrn.edu.br>
2024-08-27 11:08:53 -0300

    27.08.2024

--- Meu Game 3D/Assets/SCRIPTS/PLAYER.cs
+++ Meu Game 3D/Assets/SCRIPTS/PLAYER.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
@@ -9,6 +11,7 @@ public class PLAYER : MonoBehaviour
 {
     public int velocidade = 10;
     public int ForcaPulo = 7;
+    public bool noChao;
     public Rigidbody rb;
 
     void Start()
@@ -17,6 +20,13 @@ public class PLAYER : MonoBehaviour
         TryGetComponent(out rb);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!noChao && collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }
+    }
     void Update()
     {
         Debug.Log("Update");
@@ -26,9 +36,10 @@ public class PLAYER : MonoBehaviour
         UnityEngine.Vector3 direcao = new Vector3(x, 0, y);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
         {
             rb.AddForce(Vector3.up * ForcaPulo, ForceMode.Impulse);
+            noChao = false;
         }
         
         if (transform.position.y < -5)
