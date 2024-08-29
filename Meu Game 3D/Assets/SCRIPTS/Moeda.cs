6ffe327316f07d263ba48ac970295d64ca37bc88
PriscilaSevero <priscila.severo@escolar.ifrn.edu.br>
2024-08-29 13:52:59 -0300

    29.08.24 contraturno 13:00

---
+++ Meu Game 3D/Assets/SCRIPTS/Moeda.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moeda : MonoBehaviour
+{
+    public int velocidadeGiro = 50;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //transform.Rotate();
+    }
+}
